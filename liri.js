c6a442d72b20fb6592f6bfef258fc5f6ad32ca12
Sam Wang <samuelwangkc@gmail.com>
2019-01-19 18:07:59 -0800

    removed global variables

--- liri.js
+++ liri.js
@@ -1,22 +1,22 @@
 //when saving npm installs to dependencies npm install blah -S to assign to dependancies 
-// lets split everything and create classes for each major search 
-// include the js files
-// construct it here
+//lets split everything and create classes for each major search 
 
 require("dotenv").config();
-
-//var request = require('request');
 var Spotify = require("./spotify.js");
 var Movie = require("./movie.js");
 var fs = require("fs");
 var moment = require('moment');
 
-var userInput = process.argv.splice(3).join(" "); // array of inputs in the search
-var userChoice = process.argv[2];
+init();
 
-initLiri(userChoice,userInput);
+function init() {
+    var userInput = process.argv.splice(3).join(" "); // array of inputs in the search
+    var userChoice = process.argv[2];
 
-function initLiri(input, input2){
+    initLiri(userChoice, userInput);
+}
+
+function initLiri(input, input2) {
     if (input === "concert-this") {
         //will come back
         concert();
@@ -37,23 +37,22 @@ function initLiri(input, input2){
     }
 }
 
-
 function concert() {
     console.log("concert this later");
 }
 
 function doit() {
     var read;
-    fs.readFile("random.txt", "utf8", function(err, data){
-        if(err) console.err(err);
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) console.err(err);
         read = data.split(",");
-        initLiri(read[0],read[1]);
+        initLiri(read[0], read[1]);
     });
 }
 
-function exportTF(uVal){
-    fs.appendFile("data.txt", `${process.argv[2]}, ${uVal}\n`, function(err){
-        if(err) console.err(err);
+function exportTF(uVal) {
+    fs.appendFile("data.txt", `${process.argv[2]}, ${uVal}\n`, function (err) {
+        if (err) console.err(err);
         console.log("Command exported to data.txt!");
     })
 }
